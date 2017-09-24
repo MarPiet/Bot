ac5fe07bcf17e7ca22030db452deabaf0fb964f6
Maz <markuspietila95@gmail.com>
2017-09-24 12:35:00 +0300

    asd

---
+++ ConnectFour/oma_ai.h
@@ -0,0 +1,270 @@
+#include <stdlib.h>
+#include "connect4.h"
+
+
+/*
+	Oma AI-luokka.
+*/
+class Oma_AI : public C4_AI
+{
+public:
+
+	// -----------------------------------------------------------------------
+	// Oman AI:n toteutus!
+	//
+	// Pelin tilannetta varten ks. yliluokan C4_AI muuttujat:
+	//
+	// int _board[COLUMNS][ROWS];	pelilauta, [0][0] on vasen alanurkka
+	// int _to_move;				siirtovuoro, RED tai BLUE
+	//
+	// _board:n arvoina voivat olla vakiot RED (1), BLUE (-1) tai EMPTY (0).
+	// Vakiot COLUMNS ja ROWS asettavat pelilaudan dimensiot.
+	//
+	// Move on periaatteessa vain kokonaisluku, joka kertoo
+	// mihin sarakkeesseen kiekko tiputetaan. Sarakkeet numeroidaan
+	// vasemmalta oikealle 0,1,2,...
+	//
+	// Toteuta yliluokan puhtaat virtuaalifunktiot (= 0).
+	//
+	// Voit luonnollisesti myos lisata omia funktioitasi AI-luokkaasi.
+	// Kirjoita kaikkien funktioiden toteutukset tahan samaan tiedostoon.
+	// -----------------------------------------------------------------------
+
+	Move select_best_move()
+	{
+		//Move movelist[COLUMNS];
+		//int nof_moves = gen_moves(movelist);
+
+		//// Satunnaistekoäly
+		//return movelist[rand() % nof_moves];
+
+		// TAI minimaxilla:
+		MinMaxVal retval = minimax(this, 5);
+		return retval._move;
+	}
+
+	void get_AI_name(std::string &name) const
+	{
+		name = "Kokeilubotti";
+	}
+
+	double check_horizontals(int color) {
+		double value = 0.0;
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 6; j++)		{
+
+				if (_board[i][j] == RED && _board[i + 3][j] != BLUE && _board[i + 2][j] != BLUE && _board[i + 1][j] != BLUE)
+				{
+					if (_board[i + 3][j] == RED || _board[i + 2][j] == RED || _board[i + 1][j] == RED) {
+						value += 0.25;
+						if (_board[i + 1][j] == RED && _board[i + 2][j] == RED)
+							value += 0.5;
+						if (_board[i + 2][j] == RED && _board[i + 3][j] == RED)
+							value += 0.5;
+						if (_board[i + 1][j] == RED && _board[i + 2][j] == RED && _board[i + 3][j] == RED)
+							value += 50;
+					}
+
+				}
+				
+				
+
+				if (_board[i][j] == BLUE && _board[i + 3][j] != RED && _board[i + 2][j] != RED && _board[i + 1][j] != RED)
+				{
+					if (_board[i + 3][j] == BLUE || _board[i + 2][j] == BLUE || _board[i + 1][j] == BLUE) {
+						value += 0.25;
+						if (_board[i + 1][j] == BLUE && _board[i + 2][j] == BLUE)
+							value -= 0.5;
+						if (_board[i + 2][j] == BLUE && _board[i + 3][j] == BLUE)
+							value -= 0.5;
+						if (_board[i + 1][j] == BLUE && _board[i + 2][j] == BLUE && _board[i + 3][j] == BLUE)
+							value -= 50;
+					}
+
+				}
+					
+						
+
+					
+		
+
+			}
+		}
+		return value;
+	}
+
+	double check_verticals(int color) {
+		double value = 0.0;
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 6; j++) {
+
+				//yksi alempana = sama
+				if (_board[i][j] == RED && _board[i][j - 1] == RED)
+				{
+					if (_board[i][j - 2] != RED && j > 4)
+						continue;
+					else
+						value += 0.5;
+
+					//kaksi alempana sama
+					if (_board[i][j - 2] == RED) 
+					{
+						if (j > 5)
+							continue;
+						else
+							value += 1;
+
+						if (_board[i][j - 3] == RED)
+							value += 100;
+					}
+
+				}
+
+
+
+				//yksi alempana = sama
+				if (_board[i][j] == BLUE && _board[i][j - 1] == BLUE)
+				{
+					if (_board[i][j - 2] != BLUE && j > 4)
+						continue;
+					else
+						value -= 0.5;
+
+					//kaksi alempana sama
+					if (_board[i][j - 2] == BLUE) 
+					{
+						if (j < 5)
+							continue;
+						else 
+							value -= 1;
+
+						if (_board[i][j - 3] == BLUE)
+							value -= 100;											
+
+					}
+
+
+				}
+
+
+			}
+		}
+
+		return value;
+	}
+
+	double check_diagonals(int color) {
+		double value = 0.0;
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 6; j++) {
+
+			
+					if (_board[i][j] == RED && _board[i + 3][j + 3] != BLUE && _board[i + 2][j + 2] != BLUE && _board[i + 1][j + 1] != BLUE)
+					{
+						if (_board[i + 3][j + 3] == RED || _board[i + 2][j + 2] == RED || _board[i + 1][j + 1] == RED) {
+							value += 0.25;
+							if (_board[i + 1][j + 1] == RED && _board[i + 2][j + 2] == RED)
+								value += 1;
+							if (_board[i + 2][j + 2] == RED && _board[i + 3][j + 3] == RED)
+								value += 1;
+							if (_board[i + 1][j + 1] == RED && _board[i + 2][j + 2] == RED && _board[i + 3][j + 3] == RED)
+								value += 50;
+						}
+
+					}
+
+			
+					if (_board[i][j] == BLUE && _board[i + 3][j + 3] != RED && _board[i + 2][j + 2] != RED && _board[i + 1][j + 1] != RED)
+					{
+						if (_board[i + 3][j + 3] == BLUE || _board[i + 2][j + 2] == RED || _board[i + 1][j + 1] == RED) {
+							value -= 0.25;
+							if (_board[i + 1][j + 1] == BLUE && _board[i + 2][j + 2] == BLUE)
+								value -= 1;
+							if (_board[i + 2][j + 2] == BLUE && _board[i + 3][j + 3] == BLUE)
+								value -= 1;
+							if (_board[i + 1][j + 1] == BLUE && _board[i + 2][j + 2] == BLUE && _board[i + 3][j + 3] == BLUE)
+								value -= 50;
+						}
+
+					}
+
+
+
+
+
+			}
+		}
+
+		return value;
+	}
+
+
+
+	double evaluate()
+	{
+			double value = 0.0;
+			if (_to_move == BLUE) {
+				value -= check_horizontals(BLUE);
+				value -= check_verticals(BLUE);
+				value -= check_diagonals(BLUE);
+
+
+			}
+			else {
+				value += check_horizontals(RED);
+				value += check_verticals(RED);
+				value += check_diagonals(RED);
+
+			}
+		
+
+
+	
+	
+
+		return value;
+	}
+
+	typedef struct { double _minmax; Move _move; } MinMaxVal;
+
+	MinMaxVal minimax(Oma_AI* p, int depth)
+	{
+		MinMaxVal retval;
+		Move movelist[COLUMNS];
+		int nof_moves = gen_moves(movelist);
+		retval._minmax = check_result();
+
+		// Rekursion kantatapaus 1 (peli on päättynyt)
+		if (nof_moves == 0 || retval._minmax != 0)
+			return retval;
+
+		// Rekursion kantatapaus 2 (katkaisuvyydessä)
+		if (depth == 0)
+		{
+			retval._minmax = evaluate();
+			return retval;
+		}
+
+		// Rekursioaskeleet (kokeillaan siirtovaihtoehdot)
+		MinMaxVal best_value;
+		best_value._minmax = _to_move == RED ? -1 : 1; // mahdollisimman huono alkuarvo
+		for (int i = 0; i < nof_moves; ++i)
+		{
+			make_move(movelist[i]);
+			retval = minimax(this, depth - 1);
+			undo_move(movelist[i]);
+
+			if (_to_move == RED && retval._minmax >= best_value._minmax)
+			{
+				best_value._minmax = retval._minmax;
+				best_value._move = movelist[i];
+			}
+			if (_to_move == BLUE && retval._minmax <= best_value._minmax)
+			{
+				best_value._minmax = retval._minmax;
+				best_value._move = movelist[i];
+			}
+		}
+		return best_value;
+	}
+};
